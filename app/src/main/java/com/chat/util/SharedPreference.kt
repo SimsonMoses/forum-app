221e34d2b32fbfc9c98e6d639186a148a84557e3
Simson M <sims91502@gmail.com>
2025-01-21 22:30:57 +0530

    feat: login shared preference

--- app/src/main/java/com/chat/util/SharedPreference.kt
+++ app/src/main/java/com/chat/util/SharedPreference.kt
@@ -6,7 +6,19 @@ import android.content.SharedPreferences
 class SharedPreference {
     private val fileName: String = "chat_shared_preference";
 
-    fun sharedAccessToken(context: Context,accessToken: String){
+    /** Get Access Token from the Device
+     * @param Context
+     * */
+    fun getAccessToken(context: Context): String{
+        val preference: SharedPreferences = context.getSharedPreferences(fileName,Context.MODE_PRIVATE);
+        return preference.getString("accessToken","").toString();
+    }
+
+    /**Set Access token in the Device
+     * @param Context
+     * @param Accesstoken
+     * */
+    fun setAccessToken(context: Context, accessToken: String){
         val preference: SharedPreferences = context.getSharedPreferences(fileName,Context.MODE_PRIVATE);
         preference.edit().putString("accessToken",accessToken).apply();
     }
