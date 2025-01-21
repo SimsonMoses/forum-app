4c4c353de6f12a530b2f5e5dca562de770ba223c
Simson M <sims91502@gmail.com>
2025-01-21 22:30:39 +0530

    feat: home page

---
+++ app/src/main/java/com/chat/activity/HomeActivity.kt
@@ -0,0 +1,67 @@
+package com.chat.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.chat.R
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
+
+class HomeActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+
+    lateinit var drawLayout: DrawerLayout;
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle;
+    lateinit var navigationView: NavigationView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerlayout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        // Enable the "Up" button in the action bar
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+
+        drawLayout = findViewById(R.id.drawerlayout)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawLayout, R.string.nav_open, R.string.nav_close)
+
+        drawLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView = findViewById(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+            // call logout
+        }
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
